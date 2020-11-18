7062517c92daef62395bfcb1e82cd044f5b3b0e9
Derek Borski <derek.borski@gmail.com>
2020-11-18 16:05:00 -0700

    Update settings with postgresql database olympic_analytics_tracker and ran migrations. Database has been created

--- olympic_analytics_tracker/settings.py
+++ olympic_analytics_tracker/settings.py
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'olympic_analytics_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'olympic_analytics_tracker',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
