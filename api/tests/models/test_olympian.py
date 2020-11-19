a5af5847a655c530416a8d8e045f7346592d0140
Derek Borski <derek.borski@gmail.com>
2020-11-18 21:12:45 -0700

    Add test for eventolympian model

--- api/tests/models/test_olympian.py
+++ api/tests/models/test_olympian.py
@@ -16,3 +16,12 @@ class EventModelTest(TestCase):
   def test_string_representation(self):
     self.assertEqual(str(self.event1), self.event1.name)
 
+class EventOlympianTest(TestCase):
+  def setUp(self):
+    self.event1 = Event.objects.create(name='event1', games='2016 Summer', sport='Badminton')
+    self.olympian1 = Olympian.objects.create(name='olympian1', sex='M', age=30, height=160, weight=55, team='Spain', sport='Gymnastics')
+    self.eventolympian1 = self.olympian1.eventolympian_set.create(event_id=self.event1.id, medal='Silver')
+
+  def test_instance_of_eventolympian(self):
+    self.assertEqual(self.eventolympian1.medal, 'Silver')
+
