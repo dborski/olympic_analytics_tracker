5cea300d565c3810625ceb64171fdca5734c5de5
Derek Borski <derek.borski@gmail.com>
2020-11-19 02:37:02 -0700

    Uncomment out tests, revert manage.py back to development settings

--- api/tests/models/test_olympian.py
+++ api/tests/models/test_olympian.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 from api.models import Olympian
 
 
-# class OlympianModelTest(TestCase):
-#   def setUp(self):
-#     self.olympian1 = Olympian.objects.create(name='olympian1', sex='M', age=30, height=160, weight=55, team='Spain', sport='Gymnastics')
+class OlympianModelTest(TestCase):
+  def setUp(self):
+    self.olympian1 = Olympian.objects.create(name='olympian1', sex='M', age=30, height=160, weight=55, team='Spain', sport='Gymnastics')
 
-#   def test_string_representation(self):
-#     self.assertEqual(str(self.olympian1), self.olympian1.name)
+  def test_string_representation(self):
+    self.assertEqual(str(self.olympian1), self.olympian1.name)
 
 
