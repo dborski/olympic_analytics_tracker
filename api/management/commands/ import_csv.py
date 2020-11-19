f97d6e5f680b7450a4164cb324df6469909cbc59
Derek Borski <derek.borski@gmail.com>
2020-11-19 01:16:17 -0700

    First working version of import_csv command

--- api/management/commands/ import_csv.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from api.models import Olympian, Event, EventOlympian
-
-
-class Command(BaseCommand):
-  ''
---
+++ api/management/commands/import_csv.py
@@ -0,0 +1,50 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import Olympian, Event, EventOlympian
+import csv
+import time
+
+
+file_location = './api/fixtures/olympic_data_2016.csv'
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    print('Beginning import_csv script')
+
+    with open(file_location, 'r') as csvfile:
+      reader = csv.DictReader(csvfile)
+      total_time = 0.
+      for row in reader:
+        start = time.time()
+        height = None if row['Height'] == 'NA' else int(row['Height'])
+        weight = None if row['Weight'] == 'NA' else int(row['Weight'])
+
+        olympian, oly_created = Olympian.objects.get_or_create(
+          name=row['Name'],
+          sex=row['Sex'],
+          age=int(row['Age']),
+          height=height,
+          weight=weight,
+          team=row['Team'],
+          sport=row['Sport']
+        )
+
+        event, ev_created = Event.objects.get_or_create(
+          name=row['Event'],
+          games=row['Games'],
+          sport=row['Sport']
+        )
+
+        olympian.eventolympian_set.create(
+          event_id=event.id, 
+          medal=row['Medal']
+        )
+        stop = time.time()
+        total_time += stop - start
+
+    print(f'import_csv script has completed in {round(total_time, 2)} seconds')
+
+
+
+
+      
