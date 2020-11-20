da816152f56ba3ce36b72ab2d7c1914c7d48a11b
Derek Borski <derek.borski@gmail.com>
2020-11-20 13:59:54 -0700

    Revert settings after migration

--- api/tests/models/test_olympian.py
+++ api/tests/models/test_olympian.py
@@ -3,52 +3,52 @@ from api.models import Olympian
 from ..factories import OlympianFactory, EventFactory, EventOlympianFactory
 
 
-# class OlympianModelTest(TestCase):
-#   def setUp(self):
-#     self.olympian1 = OlympianFactory(name='Curtis')
-#     self.olympian2 = OlympianFactory(name='Albert')
-#     self.olympian3 = OlympianFactory(name='Billy')
+class OlympianModelTest(TestCase):
+  def setUp(self):
+    self.olympian1 = OlympianFactory(name='Curtis')
+    self.olympian2 = OlympianFactory(name='Albert')
+    self.olympian3 = OlympianFactory(name='Billy')
 
-#     self.event1 = EventFactory()
-#     self.event2 = EventFactory()
+    self.event1 = EventFactory()
+    self.event2 = EventFactory()
 
-#     self.event_olympian1 = EventOlympianFactory(event=self.event1, olympian=self.olympian1, medal='NA')
-#     self.event_olympian2 = EventOlympianFactory(event=self.event2, olympian=self.olympian1, medal='Bronze')
+    self.event_olympian1 = EventOlympianFactory(event=self.event1, olympian=self.olympian1, medal='NA')
+    self.event_olympian2 = EventOlympianFactory(event=self.event2, olympian=self.olympian1, medal='Bronze')
 
-#     self.event_olympian3 = EventOlympianFactory(event=self.event1, olympian=self.olympian2, medal='Silver')
-#     self.event_olympian4 = EventOlympianFactory(event=self.event2, olympian=self.olympian2, medal='Silver')
+    self.event_olympian3 = EventOlympianFactory(event=self.event1, olympian=self.olympian2, medal='Silver')
+    self.event_olympian4 = EventOlympianFactory(event=self.event2, olympian=self.olympian2, medal='Silver')
 
-#     self.event_olympian5 = EventOlympianFactory(event=self.event1, olympian=self.olympian3, medal='NA')
-#     self.event_olympian6 = EventOlympianFactory(event=self.event2, olympian=self.olympian3, medal='NA')
+    self.event_olympian5 = EventOlympianFactory(event=self.event1, olympian=self.olympian3, medal='NA')
+    self.event_olympian6 = EventOlympianFactory(event=self.event2, olympian=self.olympian3, medal='NA')
 
-#   def test_string_representation(self):
-#     self.assertEqual(str(self.olympian1), self.olympian1.name)
+  def test_string_representation(self):
+    self.assertEqual(str(self.olympian1), self.olympian1.name)
 
-#   def test_all_olympians(self):
-#     expected = [
-#       {
-#           'name': self.olympian2.name,
-#           'team': self.olympian2.team,
-#           'age': self.olympian2.age,
-#           'sport': self.olympian2.sport,
-#           'total_medals_won': 2
-#       },
-#       {
-#           'name': self.olympian3.name,
-#           'team': self.olympian3.team,
-#           'age': self.olympian3.age,
-#           'sport': self.olympian3.sport,
-#           'total_medals_won': 0
-#       },
-#       {
-#           'name': self.olympian1.name,
-#           'team': self.olympian1.team,
-#           'age': self.olympian1.age,
-#           'sport': self.olympian1.sport,
-#           'total_medals_won': 1
-#       }
-#     ]
+  def test_all_olympians(self):
+    expected = [
+      {
+          'name': self.olympian2.name,
+          'team': self.olympian2.team,
+          'age': self.olympian2.age,
+          'sport': self.olympian2.sport,
+          'total_medals_won': 2
+      },
+      {
+          'name': self.olympian3.name,
+          'team': self.olympian3.team,
+          'age': self.olympian3.age,
+          'sport': self.olympian3.sport,
+          'total_medals_won': 0
+      },
+      {
+          'name': self.olympian1.name,
+          'team': self.olympian1.team,
+          'age': self.olympian1.age,
+          'sport': self.olympian1.sport,
+          'total_medals_won': 1
+      }
+    ]
     
-#     self.assertEqual(Olympian.all_olympians(), expected)
+    self.assertEqual(Olympian.all_olympians(), expected)
 
 
--- api/tests/views/test_olympian_views.py
+++ api/tests/views/test_olympian_views.py
@@ -3,55 +3,55 @@ from django.test import TestCase
 from ..factories import OlympianFactory, EventFactory, EventOlympianFactory
 
 
-# class OlympianViewSet(TestCase):
-#   def setUp(self):
-#     self.olympian1 = OlympianFactory(name='Curtis')
-#     self.olympian2 = OlympianFactory(name='Albert')
-#     self.olympian3 = OlympianFactory(name='Billy')
+class OlympianViewSet(TestCase):
+  def setUp(self):
+    self.olympian1 = OlympianFactory(name='Curtis')
+    self.olympian2 = OlympianFactory(name='Albert')
+    self.olympian3 = OlympianFactory(name='Billy')
 
-#     self.event1 = EventFactory()
-#     self.event2 = EventFactory()
+    self.event1 = EventFactory()
+    self.event2 = EventFactory()
 
-#     self.event_olympian1 = EventOlympianFactory(event=self.event1, olympian=self.olympian1, medal='NA')
-#     self.event_olympian2 = EventOlympianFactory(event=self.event2, olympian=self.olympian1, medal='Bronze')
+    self.event_olympian1 = EventOlympianFactory(event=self.event1, olympian=self.olympian1, medal='NA')
+    self.event_olympian2 = EventOlympianFactory(event=self.event2, olympian=self.olympian1, medal='Bronze')
 
-#     self.event_olympian3 = EventOlympianFactory(event=self.event1, olympian=self.olympian2, medal='Silver')
-#     self.event_olympian4 = EventOlympianFactory(event=self.event2, olympian=self.olympian2, medal='Silver')
+    self.event_olympian3 = EventOlympianFactory(event=self.event1, olympian=self.olympian2, medal='Silver')
+    self.event_olympian4 = EventOlympianFactory(event=self.event2, olympian=self.olympian2, medal='Silver')
 
-#     self.event_olympian5 = EventOlympianFactory(event=self.event1, olympian=self.olympian3, medal='NA')
-#     self.event_olympian6 = EventOlympianFactory(event=self.event2, olympian=self.olympian3, medal='NA')
+    self.event_olympian5 = EventOlympianFactory(event=self.event1, olympian=self.olympian3, medal='NA')
+    self.event_olympian6 = EventOlympianFactory(event=self.event2, olympian=self.olympian3, medal='NA')
 
 
-#   def test_happy_path_get_all_olympians(self):
-#     response = self.client.get('/api/v1/olympians')
+  def test_happy_path_get_all_olympians(self):
+    response = self.client.get('/api/v1/olympians')
 
-#     json_response = response.json()
+    json_response = response.json()
 
-#     expected = {
-#       'olympians': [
-#         {
-#           'name': self.olympian2.name,
-#           'team': self.olympian2.team,
-#           'age': self.olympian2.age,
-#           'sport': self.olympian2.sport,
-#           'total_medals_won': 2
-#         },
-#         {
-#           'name': self.olympian3.name,
-#           'team': self.olympian3.team,
-#           'age': self.olympian3.age,
-#           'sport': self.olympian3.sport,
-#           'total_medals_won': 0
-#         },
-#         {
-#           'name': self.olympian1.name,
-#           'team': self.olympian1.team,
-#           'age': self.olympian1.age,
-#           'sport': self.olympian1.sport,
-#           'total_medals_won': 1
-#         }
-#       ]
-#     }
+    expected = {
+      'olympians': [
+        {
+          'name': self.olympian2.name,
+          'team': self.olympian2.team,
+          'age': self.olympian2.age,
+          'sport': self.olympian2.sport,
+          'total_medals_won': 2
+        },
+        {
+          'name': self.olympian3.name,
+          'team': self.olympian3.team,
+          'age': self.olympian3.age,
+          'sport': self.olympian3.sport,
+          'total_medals_won': 0
+        },
+        {
+          'name': self.olympian1.name,
+          'team': self.olympian1.team,
+          'age': self.olympian1.age,
+          'sport': self.olympian1.sport,
+          'total_medals_won': 1
+        }
+      ]
+    }
 
-#     self.assertEqual(response.status_code, 200)
-#     self.assertEqual(json_response, expected)
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(json_response, expected)
