ccdb81e44d4fb1d83cb2241f52949f3fc2a09ff3
Derek Borski <derek.borski@gmail.com>
2020-11-23 13:54:22 -0700

    Fix typo of medal

--- api/tests/models/test_event_olympian.py
+++ api/tests/models/test_event_olympian.py
@@ -11,7 +11,7 @@ class EventOlympianTest(TestCase):
   def test_instance_of_eventolympian(self):
     self.assertEqual(self.eventolympian1.medal, 'Silver')
 
-  def test_model_choices(self):
+  def test_medal_choices(self):
     expected = ['Gold', 'Silver', 'Bronze']
 
     self.assertEqual(EventOlympian.medal_choices(), expected)
