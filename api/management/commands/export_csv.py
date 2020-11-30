e8f8229ecc6966f2e6b89a53e7eb6798b0cedd65
Derek Borski <derek.borski@gmail.com>
2020-11-30 10:23:38 -0700

    Add time bencharmking to export_csv script

--- api/management/commands/export_csv.py
+++ api/management/commands/export_csv.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from api.models import Olympian
 import csv
 from datetime import date
+import time
 
 
 class Command(BaseCommand):
@@ -9,16 +10,19 @@ class Command(BaseCommand):
     self.file_location = f"./api/fixtures/olympians-{date.today().strftime('%Y-%m-%d')}.csv"
 
   def handle(self, *args, **options):
-    print('Now starting export_csv script....')
+    print('Running export_csv script....')
 
     olympians = Olympian.all_olympians()[1]
 
     with open(self.file_location, 'w', newline='') as csvfile:
       field_names = ['Name', 'Sex', 'Age', 'Height', 'Weight', 'Team', 'Sport', 'Medals Won']
       writer = csv.DictWriter(csvfile, fieldnames=field_names, dialect='excel')
+      total_time = 0.
 
       writer.writeheader()
       for olympian in olympians:
+        start = time.time()
+
         writer.writerow(
           {
             'Name': olympian.name,
@@ -31,6 +35,11 @@ class Command(BaseCommand):
             'Medals Won': olympian.medal_count
           }
         )
+
+        stop = time.time()
+        total_time += (stop - start)
+      
+      print(f'export_csv script completed in {round(total_time, 3)} seconds')
 
 
 
